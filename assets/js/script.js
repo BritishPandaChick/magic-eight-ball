24d7dfe1c5a7c9a25823c9ace09736bbda33aa22
BritishPandaChick <sbartley1080@gmail.com>
2022-03-09 13:58:17 -0500

    Add comments. Move link to js file in index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
    magic8Ball.listOfAnswers = ["It is certain", "Try again", "How would I know that?", "Yes", "No way!", "Doubt that.", "Taking a break. Will answer later."];
    $("#answer").hide();
    
+   //Displays random answer to user's question
 	magic8Ball.askQuestion = function(question){
 	  $("#answer").fadeIn(4000);
 	  var randomNumber = Math.random();
@@ -14,6 +15,7 @@ $(document).ready(function(){
           console.log(answer);
 	};
 	
+   //Get button to work and let users ask a question
    var onClick = function(){
 	 $("#answer").hide();	 
 	 $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballAnswer.png");
